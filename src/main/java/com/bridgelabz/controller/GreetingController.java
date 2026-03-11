41769f79ceffc9d2231f07ed05a851e414a45ff5
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-03-11 14:31:33 +0530

    [Varsha] : Added : UC3 : Greeting API with firstName and lastName parameters.

--- src/main/java/com/bridgelabz/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -2,8 +2,7 @@ package com.bridgelabz.controller;
 
 import com.bridgelabz.greeting_app.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -12,7 +11,10 @@ public class GreetingController {
     private GreetingService greetingService;
 
     @GetMapping("/greeting")
-    public String getGreeting() {
-        return greetingService.getGreetingMessage();
+    public String getGreeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+
+        return greetingService.getGreetingMessage(firstName, lastName);
     }
 }
--- src/main/java/com/bridgelabz/greeting_app/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greeting_app/service/GreetingService.java
@@ -5,7 +5,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
-    public String getGreetingMessage() {
+    public String getGreetingMessage(String firstName, String lastName) {
+
+        if (firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName;
+        }
+
+        if (firstName != null) {
+            return "Hello " + firstName;
+        }
+
+        if (lastName != null) {
+            return "Hello " + lastName;
+        }
+
         return "Hello World";
     }
 }
